7c5024da0fafd3d46a1b66353d564761e8259af5
Nick-labs <nikita.fomin.0404@ya.ru>
2021-01-10 03:16:46 +0300

    Nothing)

---
+++ main.py
@@ -0,0 +1,187 @@
+import pygame
+import random
+
+WIDTH, HEIGHT = 800, 800
+FPS = 60
+
+FLOOR = HEIGHT * 8 // 10
+ROCK_SIZE = (100, 100)
+
+GRAVITY = 0.25
+PLAYER_SPEED = 6
+PLAYER_JUMP_FORCE = -8
+
+FALL_TIME = 1500  # задержка между камнями в миллисекундах
+
+
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(pygame.image.load('data/flax.png'), ROCK_SIZE)
+        self.orig_image = self.image.copy()
+        self.rect = self.image.get_rect()
+
+        self.rect.left = random.randint(0, WIDTH - self.rect.width)
+        self.rect.bottom = 0
+
+        self.touches = 0
+
+        self.vx = random.randint(-5, 5)
+        self.vy = 0
+
+    def update(self):
+        self.vy += GRAVITY
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+
+        if pygame.sprite.collide_mask(self, player):
+            player.kill()
+
+        # if pygame.sprite.spritecollide(self, player_group, False):
+        #     player.kill()
+
+        if self.rect.left <= 0 or self.rect.right >= WIDTH:
+            self.vx = -self.vx
+
+        if self.touches < 2:
+            if self.rect.bottom >= FLOOR:
+                self.touches += 1
+                self.rect.bottom = FLOOR
+                self.vy = -self.vy * 0.6
+        elif self.rect.top >= HEIGHT:
+            self.kill()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(pygame.image.load('data/box.jpg'), (40, 80))
+        self.rect = self.image.get_rect()
+        self.step = 3
+        self.vx = 0
+        self.vy = 0
+
+    def calc_grav(self):
+        if self.vy == 0:
+            self.vy = 1
+        else:
+            self.vy += GRAVITY
+
+        # Если уже на земле, то ставим позицию Y как 0
+        if self.rect.bottom >= FLOOR and self.vy >= 0:
+            self.vy = 0
+            self.rect.bottom = FLOOR
+
+    def go_left(self):
+        self.vx = -PLAYER_SPEED
+        # self.image = pygame.image.load('images/player_left.png')
+        # if self.right:  # Проверяем куда он смотрит
+        #     self.right = False
+
+    def go_right(self):
+        self.vx = PLAYER_SPEED
+        # self.image = pygame.image.load('images/player_right.png')
+        # if not self.right:
+        #     self.right = True
+
+    def stop(self):
+        # self.image = pygame.image.load('images\\player_stop.png')
+        self.vx = 0
+
+    def jump(self):
+        if self.rect.bottom >= FLOOR:
+            self.vy = PLAYER_JUMP_FORCE
+
+    def update(self):
+        self.calc_grav()
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+
+        if player.rect.right > WIDTH:
+            player.rect.right = WIDTH
+
+        if player.rect.left < 0:
+            player.rect.left = 0
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Dodging rocks')
+
+clock = pygame.time.Clock()
+font = pygame.font.SysFont('arial', 40)
+
+bg = pygame.transform.scale(pygame.image.load('data/bg.jpg'), (WIDTH, FLOOR))
+floor = pygame.image.load('data/floor.jpg')
+floor = pygame.transform.scale(floor, (WIDTH, floor.get_height()))
+
+player = Player()
+player_group = pygame.sprite.Group(player)
+rocks_group = pygame.sprite.Group()
+
+last_time = 0
+time = 0
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.go_left()
+
+            if event.key == pygame.K_RIGHT:
+                player.go_right()
+
+            if event.key == pygame.K_UP:
+                player.jump()
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT and player.vx < 0:
+                player.stop()
+
+            if event.key == pygame.K_RIGHT and player.vx > 0:
+                player.stop()
+
+    if player.rect.right > WIDTH:
+        player.rect.right = WIDTH
+
+    if player.rect.left < 0:
+        player.rect.left = 0
+
+    now = pygame.time.get_ticks()
+
+    if now - last_time >= FALL_TIME:
+        rocks_group.add(Rock())
+        last_time = now
+
+    player_group.update()
+    rocks_group.update()
+
+    screen.fill((0, 0, 0))
+    screen.blit(bg, (0, 0))
+
+    player_group.draw(screen)
+    rocks_group.draw(screen)
+
+    # screen.blit(bg, (0, FLOOR))
+    screen.blit(floor, (0, FLOOR))
+
+    pygame.draw.rect(screen, (255, 255, 255), (0, HEIGHT - 50, 125, HEIGHT))
+
+    if player_group:
+        screen.blit(font.render(f'{now / 1000:06.1f}s', True, (0, 0, 0)), (5, HEIGHT - 48))
+    elif not time:
+        time = now
+    else:
+        screen.blit(font.render(f'{time / 1000:06.1f}s', True, (0, 0, 0)), (5, HEIGHT - 48))
+
+    pygame.display.flip()
+
+    # print(clock.get_fps())
+    clock.tick(FPS)
+
+pygame.quit()
